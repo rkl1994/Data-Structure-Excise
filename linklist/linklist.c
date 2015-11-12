d2d2d23da9cba54f21707d1cf8e966b0d46b2cbb
sbrkl <ruankangle123@126.com>
2015-11-12 23:03:33 +0800

    Add more function

--- linklist/linklist.c
+++ linklist/linklist.c
@@ -5,3 +5,39 @@ int isEmpty(List L)
 {
 	return L->Next == NULL;
 }
+
+int isLast(Position P, List L)
+{
+	return P->Next == NULL;
+}
+
+Position Find(int X, List L)
+{
+	Position P;
+	P = L->Next;
+	while(P != NULL && P->Element != X)
+		P = P->Next;
+	return P;
+
+}
+
+Position FindPrevious(int X, List L)
+{
+	Position P;
+	P = L;
+	while(P ->Next !=NULL && P->Next->Element != X)
+		P = P->Next;
+	return P;
+}
+
+void Delete(int X, List L)
+{
+	Position P, TmpCell;
+	P = FindPrevious(X,L);
+	if(!isLast(P,L))
+	{
+		TmpCell = P->Next;
+		P->Next = TmpCell->Next;
+		free(TmpCell);
+	}
+}
